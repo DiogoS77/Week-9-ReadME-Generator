37d0cd4c6dca788cca6eacb9e38ec3a88bd1ba22
DiogoS77 <diogo.messi.18@hotmail.com>
2023-06-09 01:15:32 +0100

    Index.js started and modules installed

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// TODO: Include packages needed for this application
+const fs = require('fs')
+const path = require('path')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+]
