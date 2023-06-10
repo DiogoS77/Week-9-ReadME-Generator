c4c0fd6710f6147b582987eed898e9b131187631
DiogoS77 <diogo.messi.18@hotmail.com>
2023-06-10 04:00:59 +0100

    added function init and writeToFile, index.js done

--- index.js
+++ index.js
@@ -64,3 +64,21 @@ const questions = [
   }
 ];
 
+async function init() {
+    try {
+      console.log('Generating README...');
+      const inquirerResponses = await inquirer.prompt(questions);
+      const markdown = generateMarkdown({ ...inquirerResponses });
+      writeToFile('README.md', markdown);
+      console.log('README generated successfully!');
+    } catch (error) {
+      console.error('An error occurred while generating the README:', error);
+    }
+  }
+  
+  function writeToFile(fileName, data) {
+    const filePath = path.join(process.cwd(), fileName);
+    fs.writeFileSync(filePath, data);
+  }
+  
+  init();
