e101f6c7e01c961385844332326b88027da7e817
DiogoS77 <diogo.messi.18@hotmail.com>
2023-06-10 18:51:08 +0100

    added render license functions and added elements

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return license !== 'None' ? `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)` : '';
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return license !== 'None' ? '\n* [License](#license)\n' : '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return license !== 'None' ? `## License\n\nThis project is licensed under the ${license} license.` : '';
+}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+To install the necessary dependencies, run the following command:
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+To run tests, use the following command:
+\`\`\`
+${data.test}
+\`\`\`
+
+${licenseSection}
+
+## Questions
+For any questions or inquiries, feel free to reach out to me:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 `;
 }
 
